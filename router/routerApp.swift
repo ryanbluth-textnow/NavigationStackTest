e7f0738cede3b5809f9bed4255e3074fe657276a
Ryan Bluth <ryan.bluth@textnow.com>
2022-05-17 11:50:57 -0400

    Initial Commit

---
+++ router/routerApp.swift
@@ -0,0 +1,17 @@
+//
+//  routerApp.swift
+//  router
+//
+//  Created by Ryan Bluth on 2022-05-17.
+//
+
+import SwiftUI
+
+@main
+struct routerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
