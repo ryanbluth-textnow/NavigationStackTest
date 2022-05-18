37aa5c6e987658163baef94a1b2458ea1fd8bb6d
Ryan Bluth <ryan.bluth@textnow.com>
2022-05-18 16:37:47 -0400

    Refactoring

--- Navigation/NavigationStack.swift
+++ Navigation/NavigationStack.swift
@@ -5,59 +5,8 @@
 //  Created by Ryan Bluth on 2022-05-17.
 //
 
-import Foundation
 import SwiftUI
 import Combine
-
-class NavigationStack<ItemIdentifier: Equatable>: ObservableObject {
-    
-    fileprivate enum Action {
-        case push(ItemIdentifier)
-        case popTo(to: ItemIdentifier, stack: [ItemIdentifier])
-    }
-    
-    fileprivate var stack: [ItemIdentifier] = []
-    
-    fileprivate var actionSubject: PassthroughSubject<Action, Never>
-    
-    init(initialIdentifier: ItemIdentifier){
-        stack = [initialIdentifier]
-        actionSubject = PassthroughSubject()
-    }
-    
-    func popTo(_ identifier: ItemIdentifier) {
-        if let index = stack.lastIndex(of: identifier) {
-            if index > 0 {
-                actionSubject.send(.popTo(to: identifier, stack: stack))
-                stack = Array(stack[0...index])
-            }
-        }
-    }
-    
-    func pop() {
-        if stack.count > 1 {
-            let current = stack[stack.count - 2]
-            actionSubject.send(.popTo(to: current, stack: stack))
-            stack.removeLast()
-        }
-    }
-    
-    func popToRoot() {
-        if let first = stack.first {
-            actionSubject.send(.popTo(to: first, stack: stack))
-            stack = [first]
-        }
-    }
-    
-    func push(_ identifier: ItemIdentifier) {
-        if stack.contains(identifier){
-            fatalError("\(String(describing: identifier)) already exists in the navigation stack.")
-        }
-        
-        stack.append(identifier)
-        actionSubject.send(.push(identifier))
-    }
-}
 
 struct NavigationStackView<ItemIdentifier: Equatable, DestinationView: View>: View {
     
@@ -73,6 +22,79 @@ struct NavigationStackView<ItemIdentifier: Equatable, DestinationView: View>: Vi
     }
 }
 
+class NavigationStack<ItemIdentifier: Equatable>: ObservableObject {
+    
+    fileprivate enum Action {
+        case push(ItemIdentifier)
+        case pop(ItemIdentifier)
+    }
+    
+    fileprivate var stack: [ItemIdentifier] = []
+    fileprivate let actionSubject: PassthroughSubject<Action, Never>
+    fileprivate let navigationItemAppearedSubject: PassthroughSubject<ItemIdentifier, Never>
+    
+    private var navigationItemAppearedSubscription: AnyCancellable?
+    
+    init(initialIdentifier: ItemIdentifier){
+        stack = [initialIdentifier]
+        actionSubject = PassthroughSubject()
+        navigationItemAppearedSubject = PassthroughSubject()
+        
+        navigationItemAppearedSubscription = navigationItemAppearedSubject.scan((nil, initialIdentifier), { prev, next in
+            (prev.1, next)
+        }).sink(receiveValue: { [weak self] values in
+            guard let self = self else { return }
+            // Determine if a view is being popped by the navigation bar back button
+            if let prev = values.0, let prevIndex = self.stack.lastIndex(of: prev), let nextIndex = self.stack.lastIndex(of: values.1) {
+                if prevIndex > nextIndex{
+                    self.pop()
+                }
+            }
+        })
+    }
+    
+    func popTo(_ identifier: ItemIdentifier) {
+        if let index = stack.lastIndex(of: identifier) {
+            if index > 0 {
+                if let toIndex = stack.lastIndex(of: identifier) {
+                    actionSubject.send(.pop(stack[toIndex + 1]))
+                }
+                stack = Array(stack[0...index])
+            }
+        }
+    }
+    
+    func pop() {
+        if let lastItem = stack.last, stack.count > 1 {
+            actionSubject.send(.pop(lastItem))
+            stack.removeLast()
+        }
+    }
+    
+    func popToRoot() {
+        if let first = stack.first, stack.count > 1 {
+            actionSubject.send(.pop(stack[1]))
+            stack = [first]
+        }
+    }
+    
+    func push(_ identifier: ItemIdentifier) {
+        if stack.contains(identifier){
+            fatalError("\(String(describing: identifier)) already exists in the navigation stack.")
+        }
+        
+        stack.append(identifier)
+        actionSubject.send(.push(identifier))
+    }
+    
+    func viewAppeared(_ identifier: ItemIdentifier) {
+        if stack.contains(identifier) {
+            
+        }
+    }
+}
+
+
 struct NavigationStackLink<DestinationView: View, Label: View, ItemIdentifier: Equatable>: View {
     
     @EnvironmentObject var navigationStack: NavigationStack<ItemIdentifier>
@@ -82,7 +104,6 @@ struct NavigationStackLink<DestinationView: View, Label: View, ItemIdentifier: E
     let destinationIdentifier: ItemIdentifier
     
     @State private var isActive: Bool = false
-    @State private var wasShown: Bool = false
     
     public init(destinationIdentifier: ItemIdentifier, @ViewBuilder destination: @escaping () -> DestinationView, @ViewBuilder label: @escaping () -> Label) {
         self.destinationIdentifier = destinationIdentifier
@@ -97,33 +118,26 @@ struct NavigationStackLink<DestinationView: View, Label: View, ItemIdentifier: E
                 self.isActive = true
             }, label: label)
             
-            NavigationLink(isActive: $isActive, destination: destination ){
+            NavigationLink(isActive: $isActive, destination: {
+                destination().onAppear {
+                    navigationStack.navigationItemAppearedSubject.send(destinationIdentifier)
+                }
+            } ){
                 EmptyView()
             }
             .isDetailLink(false)
             .onReceive(navigationStack.actionSubject) { action in
-                    switch action {
-                    case .popTo(let to, let stack):
-                        if let toIndex = stack.lastIndex(of: to) {
-                            if stack[toIndex + 1] == destinationIdentifier {
-                                isActive = false
-                            }
-                        }
-                    case .push(let identifier):
-                        if identifier == destinationIdentifier {
-                            isActive = true
-                        }
-                        break
+                switch action {
+                case .pop(let identifier):
+                    if identifier == destinationIdentifier {
+                        isActive = false
                     }
-                }
-            .onAppear {
-                // If onAppear is called after wasShown is true we can assume the back button is being pressed
-                if wasShown {
-                    if navigationStack.stack.contains(destinationIdentifier){
-                        navigationStack.pop()
+                case .push(let identifier):
+                    if identifier == destinationIdentifier {
+                        isActive = true
                     }
+                    break
                 }
-                wasShown = true
             }
         }
     }
