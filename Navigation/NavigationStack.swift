dd571488a22c295663fff0809aaa434121e42faa
Ryan Bluth <ryan.bluth@textnow.com>
2022-05-18 13:49:53 -0400

    Fixed multi pop animation issue

--- Navigation/NavigationStack.swift
+++ Navigation/NavigationStack.swift
@@ -7,31 +7,44 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class NavigationStack<Name: Equatable>: ObservableObject {
     
-    @Published fileprivate var stack: [Name] = []
+    enum Action {
+        case push(name: Name)
+        case popTo(to: Name, stack: [Name])
+    }
+    
+    fileprivate var stack: [Name] = []
+    
+    fileprivate var actionSubject: PassthroughSubject<Action, Never>
     
     init(initialName: Name){
         stack = [initialName]
+        actionSubject = PassthroughSubject()
     }
     
     func popTo(_ name: Name) {
         if let index = stack.lastIndex(of: name) {
             if index > 0 {
+                actionSubject.send(.popTo(to: name, stack: stack))
                 stack = Array(stack[0...index])
             }
         }
     }
     
     func pop() {
-        if !stack.isEmpty {
+        if stack.count > 1 {
+            let current = stack[stack.count - 2]
+            actionSubject.send(.popTo(to: current, stack: stack))
             stack.removeLast()
         }
     }
     
     func popToRoot() {
         if let first = stack.first {
+            actionSubject.send(.popTo(to: first, stack: stack))
             stack = [first]
         }
     }
@@ -42,6 +55,7 @@ class NavigationStack<Name: Equatable>: ObservableObject {
         }
         
         stack.append(name)
+        actionSubject.send(.push(name: name))
     }
 }
 
@@ -62,6 +76,7 @@ struct NavigationStackView<Name: Equatable, Destination: View>: View {
 struct NavigationStackLink<Destination: View, Label: View, Name: Equatable>: View {
     
     @EnvironmentObject var router: NavigationStack<Name>
+    @Environment(\.presentationMode) var presentation
     
     let destination: () -> Destination
     let label: () -> Label
@@ -77,7 +92,7 @@ struct NavigationStackLink<Destination: View, Label: View, Name: Equatable>: Vie
     }
     
     var body: some View {
-        ZStack {
+        Group {
             Button(action: {
                 self.router.push(self.destinationName)
                 self.isActive = true
@@ -87,9 +102,23 @@ struct NavigationStackLink<Destination: View, Label: View, Name: Equatable>: Vie
                 EmptyView()
             }
             .isDetailLink(false)
-            .onReceive(router.$stack, perform: {
-                self.isActive = $0.contains(self.destinationName)
-            })
+            .onReceive(router.actionSubject) { action in
+               
+                    switch action {
+                    case .popTo(let to, let stack):
+                        if let toIndex = stack.lastIndex(of: to) {
+                            if stack[toIndex + 1] == destinationName {
+                                isActive = false
+                            }
+                        }
+                    case .push(let name):
+                        if name == destinationName {
+                            isActive = true
+                        }
+                        break
+                    }
+                }
+            
             .onAppear {
                 // If onAppear is called after wasShown is true we can assume the back button is being pressed
                 if wasShown {
